7ee65718bb86ea689686ae09174cb77f3e87005e
Ivan Rodriguez <142862045+RyunaPhaseblade@users.noreply.github.com>
2026-03-10 22:48:30 -0600

    Add files via upload

---
+++ Window.h
@@ -0,0 +1,57 @@
+#pragma once
+#include<stdio.h>
+#include<glew.h>
+#include<glfw3.h>
+
+class Window
+{
+public:
+	Window();
+	Window(GLint windowWidth, GLint windowHeight);
+	int Initialise();
+	GLfloat getBufferWidth() { return bufferWidth; }
+	GLfloat getBufferHeight() { return bufferHeight; }
+	bool getShouldClose() {
+		return  glfwWindowShouldClose(mainWindow);}
+	bool* getsKeys() { return keys; }
+	GLfloat getXChange();
+	GLfloat getYChange();
+	void swapBuffers() { return glfwSwapBuffers(mainWindow); }
+	GLfloat getrotay() { return rotay; }
+	GLfloat getrotax() { return rotax; }
+	GLfloat getrotaz() { return rotaz; }
+	GLfloat getarticulacion1() { return articulacion1; }
+	GLfloat getarticulacion2() { return articulacion2; }
+	GLfloat getarticulacion3() { return articulacion3; }
+	GLfloat getarticulacion4() { return articulacion4; }
+	GLfloat getarticulacion5() { return articulacion5; }
+	GLfloat getarticulacion6() { return articulacion6; }
+	GLfloat getart7() { return art7; }
+	GLfloat getart8() { return art8; }
+	GLfloat getart9() { return art9; }
+	GLfloat getart10() { return art10; }
+	GLfloat getart11() { return art11; }
+	GLfloat getart12() { return art12; }
+	GLfloat getart13() { return art13; }
+	GLfloat getart14() { return art14; }
+	GLfloat getart15() { return art15; }
+	GLfloat getart16() { return art16; }
+
+	~Window();
+private: 
+	GLFWwindow *mainWindow;
+	GLint width, height;
+	GLfloat rotax,rotay,rotaz, articulacion1, articulacion2, articulacion3, articulacion4, articulacion5, articulacion6;
+	GLfloat art7, art8, art9, art10,art11,art12,art13,art14,art15,art16;
+	bool keys[1024];
+	GLint bufferWidth, bufferHeight;
+	GLfloat lastX;
+	GLfloat lastY;
+	GLfloat xChange;
+	GLfloat yChange;
+	bool mouseFirstMoved;
+	void createCallbacks();
+	static void ManejaTeclado(GLFWwindow* window, int key, int code, int action, int mode);
+	static void ManejaMouse(GLFWwindow* window, double xPos, double yPos);
+};
+
